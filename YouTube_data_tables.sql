f6efea4fa8aaa399cdadb2df6fad45b555bab111
nataliesmith32 <natalielsmith32@gmail.com>
2020-12-04 23:05:22 -0700

    three countries

--- YouTube_data_tables.sql
+++ YouTube_data_tables.sql
@@ -37,3 +37,23 @@ CREATE TABLE us_country (
 	SELECT * FROM ca_country
 	
 	DROP TABLE ca_country
+	
+	CREATE TABLE gb_country (
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM gb_country
+	
+	DROP TABLE gb_country
