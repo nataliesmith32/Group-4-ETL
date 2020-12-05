cd5ebdeb7bb01b39564f783cbc057e3ef008e08d
nataliesmith32 <natalielsmith32@gmail.com>
2020-12-04 22:17:01 -0700

    first pgAdmin table

---
+++ us_country_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE us_country (
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM us_country
+	
+	DROP TABLE us_country
