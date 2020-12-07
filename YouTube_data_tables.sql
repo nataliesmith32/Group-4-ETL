0eb0788c309a6b0903442b487e78fcf71b234c9b
nataliesmith32 <natalielsmith32@gmail.com>
2020-12-06 22:41:33 -0700

    finalizing project

--- YouTube_data_tables.sql
+++ YouTube_data_tables.sql
@@ -1,4 +1,5 @@
 CREATE TABLE us_country (
+	youtube_entries INT PRIMARY KEY,
 	video_id TEXT, 
 	trending_date VARCHAR(60), 
 	title TEXT, 
@@ -57,3 +58,45 @@ CREATE TABLE us_country (
 	SELECT * FROM gb_country
 	
 	DROP TABLE gb_country
+	
+	CREATE TABLE worlds_table (
+	youtube_entries INT PRIMARY KEY,
+	video_id TEXT, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	category_id INT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	likes INTEGER NOT NULL, 
+	dislikes INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	category TEXT,
+	country TEXT
+	);
+	
+	SELECT * FROM worlds_table
+	
+	DROP TABLE worlds_table
+	
+	CREATE TABLE worlds_category (
+	category_id INT,
+	video_id TEXT PRIMARY KEY, 
+	trending_date VARCHAR(60), 
+	title TEXT, 
+	channel_title TEXT,
+	publish_time VARCHAR(60), 
+	tags TEXT, 
+	views INTEGER NOT NULL, 
+	comment_count INTEGER NOT NULL,
+	country TEXT, 
+	category TEXT,
+	total_feedback INT NOT NULL 
+	);
+	
+	SELECT * FROM worlds_category
+	
+	DROP TABLE worlds_category
+	
+	
